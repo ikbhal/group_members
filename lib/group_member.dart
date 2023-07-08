fb86a99bba1029119239a221f867f821f653bf2f
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-08 20:32:00 +0530

    group members

---
+++ lib/group_member.dart
@@ -0,0 +1,15 @@
+// class GroupMember {
+//   String name;
+//   String mobileNumber;
+//   String? subTitle;
+//   String? profileImage;
+//   bool isAdmin;
+
+//   GroupMember({
+//     required this.name,
+//     required this.mobileNumber,
+//     this.subTitle,
+//     this.profileImage,
+//     this.isAdmin = false,
+//   });
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GroupMember {
+  String name;
+  String mobileNumber;
+  String? subTitle;
+  String? profileImage;
+  bool isAdmin;
 
-  // This widget is the root of your application.
+  GroupMember({
+    required this.name,
+    required this.mobileNumber,
+    this.subTitle ='',
+    this.profileImage ='',
+    this.isAdmin = false,
+  });
+}
+
+class TodoApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _TodoAppState createState() => _TodoAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _TodoAppState extends State<TodoApp> {
+  List<GroupMember> groupMembers = [];
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _mobileController = TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  AudioPlayer audioPlayer = AudioPlayer();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void addGroupMember(GroupMember member) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      groupMembers.add(member);
     });
   }
 
+  void deleteGroupMember(int index) {
+    setState(() {
+      groupMembers.removeAt(index);
+    });
+  }
+
+  void viewGroupMember(GroupMember member) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(member.name),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Mobile Number: ${member.mobileNumber}'),
+              Text('Subtitle: ${member.subTitle ?? ''}'),
+              if (member.profileImage != null)
+                Image.network(member.profileImage!),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  List<GroupMember> searchGroupMembers(String query) {
+    return groupMembers.where((member) {
+      final name = member.name.toLowerCase();
+      final mobileNumber = member.mobileNumber.toLowerCase();
+      return name.contains(query.toLowerCase()) ||
+          mobileNumber.contains(query.toLowerCase());
+    }).toList();
+  }
+
+  // Future<void> playAudio(String audioUrl) async {
+  //   int result = await audioPlayer.play(audioUrl);
+  //   if (result == 1) {
+  //     print('Audio played successfully');
+  //   } else {
+  //     print('Error playing audio');
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Group Members',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Group Members'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        hintText: 'Name',
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _mobileController,
+                      decoration: InputDecoration(
+                        hintText: 'Mobile Number',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      String name = _nameController.text.trim();
+                      String mobile = _mobileController.text.trim();
+                      if (name.isNotEmpty && mobile.isNotEmpty) {
+                        addGroupMember(
+                          GroupMember(name: name, mobileNumber: mobile),
+                        );
+                        _nameController.clear();
+                        _mobileController.clear();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: ListView.builder(
+                itemCount: groupMembers.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final member = groupMembers[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: member.profileImage != null
+                          ? NetworkImage(member.profileImage!)
+                          : null,
+                    ),
+                    title: Text(member.name),
+                    subtitle: Text(member.subTitle ?? ''),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => deleteGroupMember(index),
+                    ),
+                    onTap: () => viewGroupMember(member),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
